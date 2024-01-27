02369ae1d9923a79e423dd191a5920c7ab50e22b
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2024-01-27 15:02:22 +0530

    Create Power using recursion .cpp

---
+++ Power using recursion .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int power(int m,int n){
+    if(n==0){
+        return 1;
+    }
+    if(n%2==0){
+        return power(m*m,n/2);
+    }
+    else{
+        return m*power(m*m,(n-1)/2);
+    }
+}
+
+int main(){
+    int x=2;
+    int y=9;
+    cout<<power(x,y);
+}
